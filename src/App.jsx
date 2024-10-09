3dfb5422474de8d0eb8a635f392c97557f84ffcf
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-09 18:44:11 +0300

    Add MUI tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import Todolist from "./components/Todolist";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Home from "./components/Home";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Container maxwidth="xl">
+    <Container maxwidth="md">
       <AppBar position="static">
         <Toolbar>
-          <Typography variant='h6'>
-            My Todos
-          </Typography>
+          <Typography variant="h6">My Todos</Typography>
         </Toolbar>
       </AppBar>
-      <Todolist />
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Home" />
+        <Tab label="Todos" />
+      </Tabs>
+      {value === 0 && <Home />}
+      {value === 1 && <Todolist />}
       <CssBaseline />
     </Container>
   );
 }
-
 export default App;
